f907983570bb8971bd204d0538967ca5b1d61f67
Chase White <chase.t.white@hotmail.com>
2022-06-27 11:57:48 -0600

    update static / add data file

--- app.js
+++ app.js
@@ -1,30 +1,9 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
-
-// Setup static and middleware
-app.use(express.static('./public'));
-
-// Set requests
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './index.html'));
-});
-
-app.get('/about', (req, res) => {
-    res.status(200).send('About Page');
-});
-
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>Resource not found</h1>');
+    res.json([{name: 'john'}, {name: 'susan'}]);
 });
 
 app.listen(5000, () => {
---
+++ data.js
@@ -0,0 +1,40 @@
+const products = [
+    {
+        id: 1,
+        name: 'albany sofa',
+        image: 'N/A',
+        price: 39.95,
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porta non pulvinar neque laoreet suspendisse interdum.'
+    },
+    {
+        id: 2,
+        name: 'entertainment center',
+        image: 'N/A',
+        price: 3,
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non curabitur gravida arcu ac tortor dignissim convallis aenean et.'
+    },
+    {
+        id: 3,
+        name: 'albany sectional',
+        image: 'N/A',
+        price: 10.99,
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum at tempor commodo ullamcorper a lacus vestibulum sed.'
+    },
+    {
+        id: 4,
+        name: 'leather sofa',
+        image: 'N/A',
+        price: 9.99,
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod nisi porta lorem mollis aliquam.'
+    }
+];
+
+const people = [
+    {id: 1, name: 'john'},
+    {id: 2, name: 'peter'},
+    {id: 3, name: 'susan'},
+    {id: 4, name: 'anna'},
+    {id: 5, name: 'emma'}
+];
+
+module.exports = {products, people};
