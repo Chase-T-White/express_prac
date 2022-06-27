becdae2cf7f849ea5501ad6cc6fe1ce6971c6ed1
Chase White <chase.t.white@hotmail.com>
2022-06-27 15:51:08 -0600

    add morgan dep

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 const express = require('express');
-const logger = require('./logger');
-const auth = require('./auth');
 
 const app = express();
-
-app.use([logger, auth]);
-
-app.get('/', (req, res) => {
-    res.send('Welcome Home!');
-});
-
-app.get('/about', (req, res) => {
-    res.send('About Page!');
-});
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000...');
