bbd33c95422cfc6683309caa473122e3d3db56d3
Chase White <chase.t.white@hotmail.com>
2022-06-27 16:57:33 -0600

    last commit

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const {people} = require('./data');
 
 const app = express();
+
+app.use(express.static('./public'));
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data: people});
+});
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000...');
